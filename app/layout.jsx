a6a55fc7f484a40355200aa8d3d87a56676b1524
James Erringham-Bruce <james@qcutapp.com>
2022-11-15 14:11:40 +0000

    added api and analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import '../styles/globals.css';
 import Navbar from '../components/common/Navbar';
 import Footer from '../components/common/Footer';
@@ -5,6 +7,7 @@ import Footer from '../components/common/Footer';
 export default function RootLayout({ children }) {
 	return (
 		<body className='flex flex-col bg-white text-black antialiased dark:bg-gray-900 dark:text-white'>
+			<Analytics />
 			<Navbar />
 			<div className='min-h-[calc(60vh)]'>{children}</div>
 			<Footer />
